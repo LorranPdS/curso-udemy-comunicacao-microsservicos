3d54cd2be6f4f588dc07b1f9e1255b5af5b2ab81
LorranPdS <lorransantospereira@yahoo.com.br>
2024-09-05 23:44:11 -0300

    subindo tudo com docker-compose

--- sales-api/app.js
+++ sales-api/app.js
@@ -4,7 +4,7 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-app.get('/api/status', (req, res) => {
+app.get("/api/status", (req, res) => {
     return res.status(200).json({
         service: "Sales-API",
         status: "up",
